a8a322683fd0e27544912006567c922a40eba23d
balthazarbb <bardia-bb@gmx.de>
2021-07-15 13:37:23 +0200

    deploying react application on Heroku

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
+require('dotenv').config()
+
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
